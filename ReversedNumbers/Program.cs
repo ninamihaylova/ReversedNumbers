4561b16a7506e4dd05f028512832bf4c5fc0dee2
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-11 21:42:08 +0300

    Initial commit

---
+++ ReversedNumbers/Program.cs
@@ -0,0 +1,13 @@
+﻿class ReservedNumbers
+{
+    public static void Main(string[]args)
+    {
+        double numberOne=double.Parse(Console.ReadLine());
+        double numberTwo=double.Parse(Console.ReadLine());
+        double numberThree=double.Parse(Console.ReadLine());
+        
+        Console.WriteLine(numberThree);
+        Console.WriteLine(numberTwo);
+        Console.WriteLine(numberOne);
+    }
+}
